ea774c55153fd78cab5049f81a973570da0edb66
LeoArabe <leandroaragao@hotmail.com>
2025-07-10 03:47:50 -0300

    first commit

---
+++ src/test/kotlin/com/estagiario/gobots/rinha_backend/RinhaBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.estagiario.gobots.rinha_backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RinhaBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
